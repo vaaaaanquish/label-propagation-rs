0f0b59077aede6a5c1b559715207deaf7a8cf420
6syun9 <6syun9@gmail.com>
2021-08-22 22:40:45 +0900

    refact

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -10,17 +10,7 @@ use label_propagation::CAMLP;
 use ndarray::Array;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-    // origin matrix data
-    let data = vec![
-        vec![0.0, 0.3, 0.0],
-        vec![0.3, 0.0, 0.0],
-        vec![0.0, 0.0, 0.0],
-    ];
-    let row_len = data.len();
-    let col_len = data[0].len();
-    let flat_data = data.into_iter().flatten().collect::<Vec<_>>();
-    let graph = Array::from_shape_vec((row_len, col_len), flat_data).unwrap();
-
+    let graph = Array::from_shape_vec((3, 3), vec![0.0, 0.3, 0.0, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0]).unwrap();  // graph matrix data
     let x = array![0, 1];  // node index
     let y = array![0, 1];  // label index
 
